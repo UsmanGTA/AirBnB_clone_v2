9944d513c69f4d5cd52dd751e257f023ba2156c5
UsmanGTA <usmangta@gmail.com>
2020-08-18 01:27:45 +0000

    added

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""UsmanJabbar.COM"""
+from fabric.api import env, run, put
+env.hosts = [web01, web02]
+
+def do_pack():
+    """
+    ---------------
+    METHOD: DO_PACK
+    ---------------
+    DESCRIPTION:
+        Creates a tar compressed file
+        locally containing the content
+        from /web_static/ and returns the
+        path of where the compressed file
+        has been stored.
+    NOTES:
+        - Output tar files are stored inside
+        the versions/ directory.
+        - Output file would be called
+        "web_static_" + "current time" + ".tgz"
+        - If the compression fails, None's returned.
+    """
+    from fabric.api import local
+    from datetime import datetime as time
+
+    file = "web_static_" + time.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+
+    local('mkdir -p versions')
+
+    try:
+        local('tar -cvzf versions/{} web_static/'.format(file))
+        path = "versions/" + file
+        return path
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    -----------------
+    METHOD: DO_DEPLOY
+    -----------------
+    DESCRIPTION:
+        This method distributes and
+        deploys an archive of web_static
+        files
+    ARGS:
+        - Takes in a string with the path
+        to the archive file
+    NOTES:
+        - If the archive_path doesn't exist,
+        returns False
+    """
+    from os.path import isfile
+
+    # Check if that file actually exists
+    if not isfile(archive_path):
+        return False
+
+    try:
+        # Extract the file name from the var 'archive_path'
+        archive = archive_path.split('/')[-1]
+
+        # Upload, uncompress and delete the archive from the web servers
+        put(archive_path, '/tmp/')
+        out = '/data/web_static/releases/{}/'.format(archive.split('.')[0])
+        run('mkdir -p {}'.format(out))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, out))
+        run('rm -rf /tmp/{}'.format(archive))
+        run('mv {}* {}'.format(out + 'web_static/', out))
+        run('rm -rf {}'.format(out + 'web_static/'))
+
+        # Del symbolic link 'current' and link extracted folder to current
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(out))
+
+        # All good, return True
+        print('New version deployed!')
+        return True
+
+    except:
+        return False
+
+def deploy():
+    """Deploys code on the server"""
+    try:
+        file_path = do_pack()
+        status = do_deploy(archive_address)
+        return status
+    except:
+        return False
