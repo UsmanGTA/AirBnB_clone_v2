9b39bd3267d79165f7074d4ad16b10cd8acb1215
UsmanGTA <usmangta@gmail.com>
2020-08-31 06:50:54 +0000

    added initial files for task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+
+def hello_world():
+    """
+    -------------------
+    METHOD: HELLO WORLD
+    -------------------
+    Description:
+        Prints out "Hello HBNB!" if
+        flask hits a ping on 0:5000/
+    Args:
+        None
+    """
+    return("Hello HBNB!\n")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
