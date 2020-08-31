8be29d4e4055f704fec96668d784c20d9a78ebed
UsmanGTA <usmangta@gmail.com>
2020-08-31 06:58:50 +0000

    added initial commit for task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Some random docstring"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world_root():
+    """
+    ------------------------
+    METHOD: HELLO WORLD ROOT
+    ------------------------
+    Description:
+        Prints out "Hello HBNB!" if
+        flask hits a ping on 0:5000/
+    Args:
+        None
+    """
+    return("Hello HBNB!\n")
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world_hbnb():
+    """
+    ------------------------
+    METHOD: HELLO WORLD HBNB
+    ------------------------
+    Description:
+        Prints out "HBNB!" if flask hits
+        a ping on 0:5000/hbnb
+    Args:
+        None
+    """
+    return("HBNB\n")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
